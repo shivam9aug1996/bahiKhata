6f9ee6c7eabe502b009ba6ec3ae8ea89ec718d19
shivam-appboxer <shivam@appboxer.com.au>
2023-12-31 00:23:45 +0530

    fixed security

--- src/app/api/lib/keys.ts
+++ src/app/api/lib/keys.ts
@@ -1,4 +1,3 @@
 const dbPassword = process.env.DB_PASSWORD;
 export const secretKey = process.env.SECRET_KEY;
-
 export const dbUrl = `mongodb+srv://shivam9aug1996:${dbPassword}@bahikhatacluster.mcviyfo.mongodb.net/`;
