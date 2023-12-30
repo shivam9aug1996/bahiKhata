daebe622856811324452f800e23fca0ac853a5ef
shivam-appboxer <shivam@appboxer.com.au>
2023-12-31 00:37:38 +0530

    fixed security

--- src/app/components/TransactionListModal.tsx
+++ src/app/components/TransactionListModal.tsx
@@ -18,7 +18,7 @@ const TransactionListModal = ({
   return (
     <>
       <Transition appear show={isTransactionsOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-50" onClose={closeModal}>
+        <Dialog as="div" className="relative" onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
