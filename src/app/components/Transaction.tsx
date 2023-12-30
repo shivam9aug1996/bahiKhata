59e0a19e106a4500d6f32ed4cc9bb2dbef85f404
shivam-appboxer <shivam@appboxer.com.au>
2023-12-30 14:05:54 +0530

    ui fixed

--- src/app/components/Transaction.tsx
+++ src/app/components/Transaction.tsx
@@ -10,13 +10,12 @@ import {
 import dynamic from "next/dynamic";
 const Sidebar = dynamic(() => import("./Sidebar"), {
   loading: () => (
-    <div className="flex h-screen">
+    <div>
       <div
         style={{ width: "100%" }}
-        id={"sidebar"}
         className={`shadow-md border bg-gray-100 fixed inset-y-0 right-0 z-500 transition-transform duration-300 ease-in-out transform overflow-auto hover:overflow-scroll max-h-full pb-10`}
       >
-        <TransactionSkeleton />
+        <div className="flex justify-center items-center h-1/2">Loading...</div>
       </div>
     </div>
   ),
