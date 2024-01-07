a4b8a97e5ab01d8f5bd43f4fa521f874941dba40
shivam-appboxer <shivam@appboxer.com.au>
2024-01-07 13:00:12 +0530

    implemented qr auth

--- src/app/api/auth/getQR/route.ts
+++ src/app/api/auth/getQR/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import QRCode from "qrcode";
 import jwt from "jsonwebtoken";
 import { secretKey } from "../../lib/keys";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 
 export async function GET(req, res) {
   if (req.method === "GET") {
@@ -12,7 +12,7 @@ export async function GET(req, res) {
     //   },
     //   secretKey
     // );
-    const token = uuidv4();
+    const token = new Date().getTime();
     const generatedUrl = await QRCode.toDataURL(token);
     console.log("kjhgfdfghjkl", token);
     return NextResponse.json(
