f11b8c706262afcdc811392ba042c66c5ec4e21f
shivam-appboxer <shivam@appboxer.com.au>
2024-01-07 13:45:20 +0530

    implemented qr auth

--- src/app/api/auth/getQR/route.ts
+++ src/app/api/auth/getQR/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 import QRCode from "qrcode";
-import jwt from "jsonwebtoken";
-import { secretKey } from "../../lib/keys";
+// import jwt from "jsonwebtoken";
+// import { secretKey } from "../../lib/keys";
 // import { v4 as uuidv4 } from "uuid";
 
-export async function GET(req, res) {
-  if (req.method === "GET") {
+export async function POST(req, res) {
+  if (req.method === "POST") {
     // const token = jwt.sign(
     //   {
     //     action: "login",
@@ -15,6 +15,31 @@ export async function GET(req, res) {
     const token = new Date().getTime()?.toString();
     const generatedUrl = await QRCode.toDataURL(token);
     console.log("kjhgfdfghjkl", token);
+    // const setCustomHeader = (res, headers) => {
+    //   Object.entries(headers).forEach(([name, value]) => {
+    //     res.headers.set(name, value);
+    //   });
+    //   return res;
+    // };
+
+    // const cacheHeader = () => {
+    //   return {
+    //     "Cache-Control": "no-store",
+    //   };
+    // };
+
+    // return setCustomHeader(
+    //   NextResponse.json(
+    //     {
+    //       message: "QR generated successfully",
+    //       data: generatedUrl,
+    //       temp: token,
+    //       timeStamp: new Date().getTime(),
+    //     },
+    //     { status: 201 }
+    //   ),
+    //   cacheHeader()
+    // );
     return NextResponse.json(
       {
         message: "QR generated successfully",
