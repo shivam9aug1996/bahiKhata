80aad888e665c2378b70e4c06315b03a3de77519
shivam-appboxer <shivam@appboxer.com.au>
2024-01-07 12:48:36 +0530

    implemented qr auth

--- src/app/api/auth/getQR/route.ts
+++ src/app/api/auth/getQR/route.ts
@@ -20,6 +20,7 @@ export async function GET(req, res) {
         message: "QR generated successfully",
         data: generatedUrl,
         temp: token,
+        timeStamp: new Date().getTime(),
       },
       { status: 201 }
     );
