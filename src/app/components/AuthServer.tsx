dc9679862890c88ed912bfa6d80b64e02fd942be
shivam-appboxer <shivam@appboxer.com.au>
2024-01-10 23:45:09 +0530

    fixed footer style and customer loader

--- src/app/components/AuthServer.tsx
+++ src/app/components/AuthServer.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const AuthServer = ({ children }) => {
   return (
-    <div className="flex items-center justify-center bg-gradient-to-r from-gray-100 to-gray-200 py-12 px-4 sm:px-6 lg:px-8">
+    <div
+      className="flex items-center justify-center bg-gradient-to-r from-gray-100 to-gray-200 py-12 px-4 sm:px-6 lg:px-8"
+      style={{ paddingBottom: 150 }}
+    >
       <div className="max-w-md w-full bg-white rounded-lg shadow-md overflow-hidden">
         {children}
       </div>
